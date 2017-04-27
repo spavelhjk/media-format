806b798a71262b1544afdd55e75d85de627e07f8
SPavelhjk <SPavel.hjk@mail.ru>
2017-04-27 16:31:15 +0600

    Filled the project

---
+++ index.js
@@ -0,0 +1,34 @@
+function timeDetect(val) {
+	return val > 60 ? val % 60 : val;
+}
+
+function timeReplace(format, obj) {
+	var key;
+	for(key in obj) {
+		format = format.replace(key, obj[key]);
+	}
+
+	return format;
+}
+
+function videoFormat(vid, format) {
+	if(typeof format !== 'string') {
+		throw new TypeError('Incorrect "format" type. The "format" must be a string').message;
+	}
+
+	let curr = vid.currentTime;
+	let durr = vid.duration;
+
+	let res = {};
+	res['|Pc|'] = ((curr / durr) * 100).toFixed(0);
+
+	res['|H|'] = parseInt( (curr / 3600) % 3600 );
+
+	res['|M|'] = timeDetect( parseInt( (curr / 60) % 60 ) );
+
+	res['|S|'] = timeDetect( parseInt( curr ) );
+
+	return timeReplace(format, res);
+}
+
+//export default videoValues;
