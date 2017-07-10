38b6f52abc78734033f0ec2b15ded145286998b9
SPavelhjk <SPavel.hjk@mail.ru>
2017-07-10 18:17:32 +0600

    Move methods into

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-function timeDetect(val) {
-	return val > 60 ? val % 60 : val;
-}
-
-function timeReplace(format, obj) {
-	var key;
-	for(key in obj) {
-		format = format.replace(key, obj[key]);
+function videoFormat(media, format) {
+	function timeDetect(val) {
+		return val > 60 ? val % 60 : val;
 	}
 
-	return format;
-}
+	function timeReplace(format, obj) {
+		var key;
+		for(key in obj) {
+			format = format.replace(key, obj[key]);
+		}
 
-function videoFormat(media, format) {
+		return format;
+	}
+
 	if(typeof format !== 'string') {
 		throw new TypeError('Incorrect "format" type. The "format" must be a string').message;
 	}
 
-	let curr = media.currentTime;
-	let durr = media.duration;
+	var curr = media.currentTime;
+	var durr = media.duration;
 
-	let res = {};
-	res['|Pc|'] = ((curr / durr) * 100).toFixed(0);
+	var res = {};
+	res['|pc|'] = ((curr / durr) * 100).toFixed(0);
 
-	res['|H|'] = parseInt( (curr / 3600) % 3600 );
+	res['|h|']  = parseInt( (curr / 3600) % 3600 );
 
-	res['|M|'] = timeDetect( parseInt( (curr / 60) % 60 ) );
+	res['|i|']  = timeDetect( parseInt( (curr / 60) % 60 ) );
 
-	res['|S|'] = timeDetect( parseInt( curr ) );
+	res['|s|']  = timeDetect( parseInt( curr ) );
 
 	return timeReplace(format, res);
 }
